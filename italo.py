b5dee48a82a06e8b624cb1cd25fa7c622166facc
italodebezerra <italobezerra354@gmail.com>
2024-06-12 14:10:23 -0300

    first commit

--- italo.py
+++ italo.py
@@ -1,6 +1,26 @@
-nome = "italo"
-idade = 16 
-altura = 1.71
-maior_de_idade = idade>=18 
-print("Nome:", nome, "idade:" , idade, "altura:", altura)
-print("É maior de idade ?", maior_de_idade)
+adicao = 10 + 10 
+print ('Adicao', adicao)
+
+subtracao = 10 - 5 
+print ('Subtracao', subtracao) 
+
+multiplicacao = 10 * 10 
+print ('Multiplicação', multiplicacao)
+
+divisao = 10 / 3 # float 
+print ('Divisão', divisao)
+
+divisao_inteira = 10 // 3
+print ('Divisão_inteira', divisao_inteira) 
+
+exponenciacao = 2 ** 10 
+print ('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto das divisião 
+print ('Módulo', modulo)
+
+print (10 % 8 == 0)
+print (16 % 8 == 0)
+print (10 % 2 == 0)
+print (15 % 2 == 0)
+print (16 % 2 == 0)
